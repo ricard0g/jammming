800b72472a693b68aa9f2185ae0eb2e67122f867
Ricardo Guzman <ricardoguzman444@gmail.com>
2024-05-08 13:23:08 +0200

    Add header without style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>Ja<span className={styles.highlight}>mmm</span>ing</h1>
+        <button className={styles.loginButton}>Login</button>
       </header>
+      <div className="App">
+      </div>
     </div>
   );
 }
