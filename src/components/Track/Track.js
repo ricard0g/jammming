ef5c6b3cd6542cadd3460f3f3f9421bacb9fdee3
Ricardo Guzman <ricardoguzman444@gmail.com>
2024-05-17 19:38:11 +0200

    Shows or not preview based on logic. Includes Spotify Icon for legal purposes

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import styles from "./Track.module.css";
+import spotifyIcon from "../../assets/Spotify_Logo_RGB_White.webp";
 
 function Track(props) {
 	const removeTrack = useCallback(
@@ -16,29 +17,44 @@ function Track(props) {
 		[props.onAdd, props.track]
 	);
 
-    const renderAction = () => {
-        if(props.isRemoval){
-            return (
-                <button className={styles.removalButton} onClick={removeTrack}>
-                    -
-                </button>
-            );
-        }
-        return (
-            <button className={styles.addButton} onClick={addTrack}>
-                +
-            </button>
-        )
-    }
+	const renderAction = () => {
+		if (props.isRemoval) {
+			return (
+				<button className={styles.removalButton} onClick={removeTrack}>
+					-
+				</button>
+			);
+		}
+		return (
+			<button className={styles.addButton} onClick={addTrack}>
+				+
+			</button>
+		);
+	};
 
 	return (
 		<div className={styles.trackContainer}>
 			<div className={styles.trackContent}>
-                <h3 className={styles.titleTrack}>{props.track.name}</h3>
-                <p className={styles.trackInfo}>{props.track.artist} | {props.track.album}</p>
-                <audio controls src={props.track.preview} className={styles.audioSample}></audio><img src='' alt="Spotify Icon" />
-            </div>
-            {renderAction()}
+				<h3 className={styles.titleTrack}>{props.track.name}</h3>
+				<p className={styles.trackInfo}>
+					{props.track.artist} | {props.track.album}
+				</p>
+				{props.track.preview ? (
+					<div className={styles.audioIconContainer}>
+						<audio
+							controls
+							src={props.track.preview}
+							className={styles.audioSample}
+						></audio>
+						<img src={spotifyIcon} alt="Spotify Icon" className={styles.icon} />
+					</div>
+				) : (
+					<div className={styles.audioIconContainer}>
+						<p className={styles.noPreview}>No Preview Available</p>
+					</div>
+				)}
+			</div>
+			{renderAction()}
 		</div>
 	);
 }
